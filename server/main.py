cc68dcd371985f28dc5d1ff7e8c4062399d7dde7
Tireon003 <gunash29@mail.ru>
2024-09-29 02:30:53 +0300

    initial main app configuration

--- server/main.py
+++ server/main.py
@@ -2,7 +2,11 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import (
     FastAPI,
+)
 
+from routes import (
+    genre_router,
+    video_router,
 )
 
 origins = [
@@ -11,13 +15,17 @@ origins = [
 ]
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
+app.include_router(genre_router)
+app.include_router(video_router)
+
 
 if __name__ == "__main__":
     uvicorn.run(
